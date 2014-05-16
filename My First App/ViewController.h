9c85ad16e259d8245295b8298d74b896d96f0642
Angelo Antonio Salatino <angelosalatino@Angelos-Mac.local>
2014-05-16 16:37:46 +0200

    Initial Commit

---
+++ My First App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  My First App
+//
+//  Created by Angelo Antonio Salatino on 16/05/2014.
+//  Copyright (c) 2014 Angelo Antonio Salatino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ My First AppTests/My_First_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  My_First_AppTests.m
+//  My First AppTests
+//
+//  Created by Angelo Antonio Salatino on 16/05/2014.
+//  Copyright (c) 2014 Angelo Antonio Salatino. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_First_AppTests : XCTestCase
+
+@end
+
+@implementation My_First_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
